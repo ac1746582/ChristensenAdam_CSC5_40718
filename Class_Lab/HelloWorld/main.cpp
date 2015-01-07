9e078415645e83a414ee51155d44c1d6152f4e8f
ac1746582 <achristens@student.rcc.edu>
2015-01-06 20:03:05 -0800

    Signed-off-by: ac1746582 <achristens@student.rcc.edu>

---
+++ Class_Lab/HelloWorld/main.cpp
@@ -0,0 +1,26 @@
+/* 
+  File:   main.cpp
+  Author: Adam Christensen
+  Created on January 6, 2015, 6:05 PM
+  Purpose: Our first program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Output Hello World
+    cout<<"Hello World"<<endl;
+    //Exit Stage Right
+    
+    return 0;
+}
+
